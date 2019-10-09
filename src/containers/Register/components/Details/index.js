9ecae2f3662266990567c22e0315e1015b23bb95
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-10-09 15:19:30 +0200

    Register details edit

--- src/containers/Register/components/Details/index.js
+++ src/containers/Register/components/Details/index.js
@@ -6,6 +6,7 @@ const Details = ({ values, onChange, onSubmit }) => (
   <div className={style.wrapper}>
     <input className={style.input} type="text" name="description" value={values.description} onChange={onChange} />
     <input className={style.input} type="text" name="note" value={values.note} onChange={onChange} />
+    <button className={style.button} onClick={onSubmit}>Submit</button>
   </div>
 );
 
--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 import Details from "./components/Details";
-import { QUERY_REGISTER_DETAIL } from "../../queries";
+import { QUERY_REGISTER_DETAIL, MUTATION_REGISTER, QUERY_REGISTER_LIST } from "../../queries";
 
-const Register = ({ match }) => {
-  const { loading, error, data } = useQuery(QUERY_REGISTER_DETAIL, { variables: { id: match.params.registerId } });
+const Register = ({ match, history }) => {
+  const registerQuery = useQuery(QUERY_REGISTER_DETAIL, { variables: { id: match.params.registerId } });
   const [input, setInput] = useState({ description: "", note: "" });
-  useEffect(() => data && setInput(data.detailRegister), [data]);
+  useEffect(() => registerQuery.data && setInput(registerQuery.data.detailRegister), [registerQuery]);
+  const [saveRegister, registerMutation] = useMutation(MUTATION_REGISTER);
+  const onSubmit = () => saveRegister({ variables: { id: match.params.registerId, description: input.description, note: input.note }, refetchQueries: [{ query: QUERY_REGISTER_LIST }]}).then(() => {
+    history.push("/registers");
+  });
   const onChange = event => setInput({ ...input, [event.target.name]: event.target.value });
   return (
     <>
-      {loading && <span>Loading...</span>}
-      {error && <span>Error!</span>}
-      {data && <Details values={input} onChange={onChange} />}
+      {registerQuery.loading || registerMutation.data && <span>Loading...</span>}
+      {registerQuery.error || registerMutation.error && <span>Error!</span>}
+      {input && <Details values={input} onChange={onChange} onSubmit={onSubmit} />}
     </>
   );
 };
