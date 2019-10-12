5a39760af7b62de63b70abbf8fc5495cf7e89663
Emanuele Scarabattoli <em.scarabattoli@gmail.com>
2019-10-12 20:38:50 +0200

    Statistics rows details

--- src/containers/Statistic/index.js
+++ src/containers/Statistic/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { useQuery, useMutation } from "@apollo/react-hooks";
+import { useQuery, useMutation, useLazyQuery } from "@apollo/react-hooks";
 
 import Details from "./components/Details";
 import ErrorMessage from "../../components/ErrorMessage";
@@ -10,7 +10,11 @@ import {
   MUTATION_STATISTICS,
   QUERY_STATISTICS_LIST,
   QUERY_STATISTICS_ROW_REGISTER_LIST,
-  QUERY_STATISTICS_ROW_STATISTICS_LIST
+  QUERY_STATISTICS_ROW_STATISTICS_LIST,
+  QUERY_STATISTICS_ROW_REGISTER_DETAIL,
+  QUERY_STATISTICS_ROW_STATISTICS_DETAIL,
+  MUTATION_STATISTICS_ROW_REGISTER,
+  MUTATION_STATISTICS_ROW_STATISTICS
 } from "../../queries";
 
 const getIdByParams = match => match && match.params && match.params.statisticId || undefined;
@@ -26,7 +30,21 @@ const Statistic = ({ match, history }) => {
   const registerRows = useQuery(QUERY_STATISTICS_ROW_REGISTER_LIST, { variables: { statistics: id }, skip: !id });
   const statisticsRows = useQuery(QUERY_STATISTICS_ROW_STATISTICS_LIST, { variables: { statistics: id }, skip: !id });
 
+  const [loadStatisticsRowRegister, statisticsRowRegisterQuery] = useLazyQuery(QUERY_STATISTICS_ROW_REGISTER_DETAIL);
+  const [saveStatisticsRowRegister, statisticsRowRegisterMutation] = useMutation(MUTATION_STATISTICS_ROW_REGISTER);
+  const [inputStatisticsRowRegister, setInputStatisticsRowRegister] = useState({ id: undefined, register: { id: "" } });
+  const [isOnEditStatisticsRowRegister, setIsOnEditStatisticsRowRegister] = useState(false);
+
+  const [loadStatisticsRowStatistics, statisticsRowStatisticsQuery] = useLazyQuery(QUERY_STATISTICS_ROW_STATISTICS_DETAIL);
+  const [saveStatisticsRowStatistics, statisticsRowStatisticsMutation] = useMutation(MUTATION_STATISTICS_ROW_STATISTICS);
+  const [inputStatisticsRowStatistics, setInputStatisticsRowStatistics] = useState({ id: undefined, statistics: { id: "" } });
+  const [isOnEditStatisticsRowStatistics, setIsOnEditStatisticsRowStatistics] = useState(false);
+
   useEffect(() => statisticQuery.data && statisticQuery.data.detailStatistics && setInput(statisticQuery.data.detailStatistics), [statisticQuery]);
+
+  useEffect(() => statisticsRowRegisterQuery.data && statisticsRowRegisterQuery.data.detailStatisticsRowRegister && setInputRow(statisticsRowRegisterQuery.data.detailStatisticsRowRegister), [statisticsRowRegisterQuery]);
+
+  useEffect(() => statisticsRowStatisticsQuery.data && statisticsRowStatisticsQuery.data.detailStatisticsRowStatistics && setInputRow(statisticsRowStatisticsQuery.data.detailStatisticsRowStatistics), [statisticsRowStatisticsQuery]);
 
   const onSubmit = () => {
     saveStatistic(
@@ -53,7 +71,31 @@ const Statistic = ({ match, history }) => {
     setIsOnEdit(true);
   };
 
+  const onClickEditStatisticsRowRegister = rowId => {
+    loadStatisticsRowRegister({ variables: { id: rowId } });
+    setIsOnEditStatisticsRowRegister(true);
+  };
+
+  const onClickNewStatisticsRowRegister = () => {
+    setInputStatisticsRowRegister({ id: undefined, register: { id: "" } });
+    setIsOnEditStatisticsRowRegister(true);
+  };
+
+  const onClickEditStatisticsRowStatistics = rowId => {
+    loadStatisticsRowStatistics({ variables: { id: rowId } });
+    setIsOnEditStatisticsRowStatistics(true);
+  };
+
+  const onClickNewStatisticsRowStatistics = () => {
+    setInputStatisticsRowStatistics({ id: undefined, statistics: { id: "" } });
+    setIsOnEditStatisticsRowStatistics(true);
+  };
+
   const onChange = event => setInput({ ...input, [event.target.name]: event.target.value });
+
+  const onChangeStatisticsRowRegister = event => setInputStatisticsRowRegister({ ...inputStatisticsRowRegister, [event.target.name]: event.target.value });
+
+  const onChangeStatisticsRowStatistics = event => setInputStatisticsRowStatistics({ ...inputStatisticsRowStatistics, [event.target.name]: event.target.value });
 
   return (
     <>
@@ -62,7 +104,11 @@ const Statistic = ({ match, history }) => {
           statisticQuery.loading ||
           statisticMutation.loading ||
           registerRows.loading ||
-          statisticsRows.loading
+          statisticsRows.loading ||
+          statisticsRowRegisterQuery.loading ||
+          statisticsRowRegisterMutation.loading ||
+          statisticsRowStatisticsQuery.loading ||
+          statisticsRowStatisticsMutation.loading
         ) &&
         <LoadingMessage />
       }
@@ -72,10 +118,22 @@ const Statistic = ({ match, history }) => {
           statisticMutation.error ||
           registerRows.error ||
           statisticsRows.error ||
+          statisticsRowRegisterQuery.error ||
+          statisticsRowRegisterMutation.error ||
+          statisticsRowStatisticsQuery.error ||
+          statisticsRowStatisticsMutation.error ||
           (
             statisticMutation.data &&
             statisticMutation.data.mutationStatistics.errors &&
             statisticMutation.data.mutationStatistics.errors.length > 0
+          ) || (
+            statisticsRowRegisterMutation.data &&
+            statisticsRowRegisterMutation.data.statisticsRowRegister.errors &&
+            statisticsRowRegisterMutation.data.statisticsRowRegister.errors.length > 0
+          ) || (
+            statisticsRowStatisticsMutation.data &&
+            statisticsRowStatisticsMutation.data.statisticsRowStatistics.errors &&
+            statisticsRowStatisticsMutation.data.statisticsRowStatistics.errors.length > 0
           )
         ) &&
         <ErrorMessage />
--- src/queries/index.js
+++ src/queries/index.js
@@ -117,6 +117,30 @@ export const QUERY_REGISTER_ROW_DETAIL = gql`
   }
 `;
 
+export const QUERY_STATISTICS_ROW_REGISTER_DETAIL = gql`
+  query detailStatisticsRowRegister($id: Int) {
+    detailStatisticsRowRegister(id: $id) {
+      id
+      register {
+        id
+        description
+      }
+    }
+  }
+`;
+
+export const QUERY_STATISTICS_ROW_STATISTICS_DETAIL = gql`
+  query detailStatisticsRowStatistics($id: Int) {
+    detailStatisticsRowStatistics(id: $id) {
+      id
+      register {
+        id
+        description
+      }
+    }
+  }
+`;
+
 export const QUERY_STATISTICS_ROW_REGISTER_LIST = gql`
   query listStatisticsRowRegister($statistics: Int) {
     listStatisticsRowRegister(statistics: $statistics) {
@@ -143,6 +167,34 @@ export const MUTATION_REGISTER_ROW = gql`
   mutation mutationRegisterRow($id: ID, $date: Date!, $description: String!, $period: String!, $amount: Float!, $note: String, $register: ID!) {
     mutationRegisterRow(input: {id: $id, date: $date, description: $description, period: $period, amount: $amount, note: $note, register: $register}) {
       registerRow {
+        id
+      }
+      errors {
+        field
+        messages
+      }
+    }
+  }
+`;
+
+export const MUTATION_STATISTICS_ROW_REGISTER = gql`
+  mutation mutationStatisticsRowRegister($id: ID, $parentStatistics: ID!, $register: ID!) {
+    mutationStatisticsRowRegister(input: {id: $id, parentStatistics: $parentStatistics, register: $register}) {
+      statisticsRowRegister {
+        id
+      }
+      errors {
+        field
+        messages
+      }
+    }
+  }
+`;
+
+export const MUTATION_STATISTICS_ROW_STATISTICS = gql`
+  mutation mutationStatisticsRowStatistics($id: ID, $parentStatistics: ID!, $statistics: ID!) {
+    mutationStatisticsRowStatistics(input: {id: $id, parentStatistics: $parentStatistics, statistics: $statistics}) {
+      statisticsRowRegister {
         id
       }
       errors {
